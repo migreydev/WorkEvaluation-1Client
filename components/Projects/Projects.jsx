a7f3d6860513387ee51663b7fcc9dcece3248adc
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-11-22 13:52:19 +0100

    implementación del paginado de proyectos

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,26 +1,44 @@
 import "./Projects.css";
-
-// Example project data
-const projects = [
-  {
-    id: 1,
-    title: "Twitter Clone",
-    description:
-      "A Twitter clone built with PHP, and MySQL. It allows users to log in, follow other users, and view posts.",
-    image: "https://via.placeholder.com/300x200.png?text=Twitter+Clone",
-    link: "https://github.com/yourusername/twitter-clone",
-  },
-  {
-    id: 2,
-    title: "E-commerce React",
-    description:
-      "An e-commerce application built with React and Redux. It allows users to add products to the cart and make payments.",
-    image: "https://via.placeholder.com/300x200.png?text=E-commerce+React",
-    link: "https://github.com/yourusername/e-commerce-react",
-  },
-];
+import React, { useEffect, useState } from "react";
 
 const Projects = () => {
+  // Inicializamos el estado de los proyectos, la página actual y el número total de páginas
+  const [projects, setProjects] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0); // Página inicial
+  const [totalPages, setTotalPages] = useState(0); // Total de páginas
+
+  // Función para cargar los proyectos
+  const fetchProjects = (page) => {
+    //Realizamos un fetch al siguiente endPoint
+    fetch(`http://localhost:8080/api/v1/projects?page=${page}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProjects(data.content); // Asignamos los proyectos de la página actual
+        setTotalPages(data.totalPages); // Asignamos el total de páginas
+      })
+      .catch((error) => {
+        console.error("Error al obtener los proyectos:", error);
+      });
+  };
+
+  // useEffect para cargar los proyectos cuando el componente se ejecuta o cuando la página cambia
+  useEffect(() => {
+    fetchProjects(currentPage);
+  }, [currentPage]);
+
+  // Funciones para manejar la pagina
+  const prevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPages - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
     <section id="projects" className="projects-section">
       <h2>My Projects</h2>
@@ -31,17 +49,25 @@ const Projects = () => {
             <h3>{project.title}</h3>
             <p>{project.description}</p>
             <div className="button-container">
-              <a
-                href={project.link}
-                className="project-button"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <a href={project.link} className="project-button">
                 View Project
               </a>
             </div>
           </div>
         ))}
+      </div>
+
+      {/* Controles de paginación */}
+      <div className="pagination-controls">
+        <button onClick={prevPage} disabled={currentPage === 0}>
+          Prev
+        </button>
+        <span>
+          Page {currentPage + 1} of {totalPages}
+        </span>
+        <button onClick={nextPage} disabled={currentPage === totalPages - 1}>
+          Next
+        </button>
       </div>
     </section>
   );
