11828381a2348fedb56ef65a16c2846849bba658
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-11-26 08:54:13 +0100

    añadiendo delete developer, implementación

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import "./Projects.css";
 import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
@@ -7,6 +8,69 @@ const Projects = () => {
   const [totalPages, setTotalPages] = useState(0); // Total de páginas
   const [searchTerm, setSearchTerm] = useState(""); // Buscar palabra
   const [errorMessage, setErrorMessage] = useState(""); // Mensaje de error para búsqueda
+
+  // Componente reutilizable para mostrar una tarjeta de proyecto
+  const ProjectCard = ({ project, test = false, onDelete }) => {
+    return (
+      <div className="project-card">
+        <h3>{project.project_name}</h3>
+        <img src={project.picture} alt={project.project_name} />
+        <p>{project.description}</p>
+        {/* Se muestran el status del proyecto */}
+        <p>
+          <b>Status:</b>
+          {project.status.status_name}
+        </p>
+        <p>
+          {/* Se muestran los desarrolladores solo si están asociados al proyecto */}
+          <b>Developer:</b>
+          {project.developersHasProjectsDTO.length === 0 && "No developers"}
+          {project.developersHasProjectsDTO.length > 0 &&
+            project.developersHasProjectsDTO
+              .map((developer) => developer.dev_name)
+              .join(", ")}
+        </p>
+        <div className="button-container">
+          <a href={project.repository_url} className="project-button">
+            View Project
+          </a>
+          {/* Si test es true, muestra el botón de borrado */}
+          {test && (
+            <button
+              onClick={() =>
+                onDelete(project.developersHasProjectsDTO[0].dev_id)
+              }
+              className="delete-button"
+            >
+              Delete
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  // Validación de campos del prop Project Card
+  ProjectCard.propTypes = {
+    project: PropTypes.shape({
+      project_id: PropTypes.number.isRequired,
+      project_name: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      repository_url: PropTypes.string.isRequired,
+      status: PropTypes.shape({
+        status_name: PropTypes.string.isRequired,
+      }).isRequired,
+      developersHasProjectsDTO: PropTypes.arrayOf(
+        PropTypes.shape({
+          dev_id: PropTypes.number.isRequired,
+          dev_name: PropTypes.string.isRequired,
+        })
+      ).isRequired,
+    }).isRequired,
+    test: PropTypes.bool,
+    onDelete: PropTypes.func.isRequired,
+  };
 
   // Función para cargar los proyectos
   const fetchProjects = (page, word = "") => {
@@ -26,8 +90,8 @@ const Projects = () => {
         setErrorMessage(""); // Limpiamos cualquier mensaje de error
       })
       .catch((error) => {
-        console.error("Error al obtener los proyectos:", error);
-        setErrorMessage("Hubo un problema al cargar los proyectos.");
+        console.error("Error in obtaining projects:", error);
+        setErrorMessage("There was a problem loading the projects.");
       });
   };
 
@@ -40,6 +104,26 @@ const Projects = () => {
       setCurrentPage(0); // Restablecemos a la primera página
       fetchProjects(currentPage); //Volvemos a cargar todos los proyectos
     }
+  };
+
+  // Función para eliminar un developer
+  const deleteDeveloper = (developerId) => {
+    fetch(`http://localhost:8080/api/v1/developers/${developerId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Recargar proyectos tras eliminar
+          fetchProjects(currentPage);
+        } else {
+          throw new Error("Failed to remove developer");
+        }
+      })
+      .catch((error) => {
+        console.log(developerId);
+        console.error("Error deleting the developer:", error);
+        setErrorMessage("There was a problem deleting the developer.");
+      });
   };
 
   // useEffect para cargar los proyectos cuando el componente se ejecuta o cuando la página cambia
@@ -78,16 +162,12 @@ const Projects = () => {
       {/* Mostrar mensaje de error */}
       <div className="projects-list">
         {projects.map((project) => (
-          <div key={project.project_id} className="project-card">
-            <h3>{project.project_name}</h3>
-            <img src={project.picture} alt={project.project_name} />
-            <p>{project.description}</p>
-            <div className="button-container">
-              <a href={project.repository_url} className="project-button">
-                View Project
-              </a>
-            </div>
-          </div>
+          <ProjectCard
+            key={project.project_id}
+            project={project}
+            test={project.developersHasProjectsDTO.length > 0} // Mostrar botón si hay developers
+            onDelete={deleteDeveloper} // Pasamos la función de borrado
+          />
         ))}
       </div>
       {/* Controles de paginación */}
